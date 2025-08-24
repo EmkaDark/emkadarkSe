5277bbc4719a4f0dd75b32d684be901963614f7d
Emka <nomad11kl@mail.ru>
2025-08-24 22:42:36 +0300

    added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ["http://emkadark.ru", "http://localhost:3000"],
+  });
   app.setGlobalPrefix("/api");
   await app.listen(process.env.PORT || 3002, () => {
     console.log(`[--server] Listening in http://localhost:${process.env.PORT}`);
